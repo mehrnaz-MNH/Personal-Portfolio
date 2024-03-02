954b88d680bbc81693c8b9cdabf9809562832803
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-03-02 14:39:24 +1000

    hero section done!

--- vite-project/src/components/Hero/Hero.jsx
+++ vite-project/src/components/Hero/Hero.jsx
@@ -4,16 +4,18 @@ import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I'm Mehrnaz</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Mehrnaz</h1>
+        <p className={styles.description}>
           I'm a Computer Science Student interested in Web and App
           Development.Reach out if you like to learn more
         </p>
-        <a href="mailto:mnh.mehrnaz@gmail.com">Contact Me</a>
+        <a href="mailto:mnh.mehrnaz@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
-      <img src={getImageURL("hero/heroImage.png")} />
+      <img src={getImageURL("hero/heroImage.png")} className={styles.heroImg} />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Mehrnaz's Portfolio
+        My Portfolio
       </a>
       <div className={styles.menu}>
         <img
