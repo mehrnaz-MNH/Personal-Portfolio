23d61e75b84d60cdcdb634a4a8bcb4f023306936
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-03-07 15:38:04 +1000

    made new changes for cleaning up purpose

--- vite-project/src/components/Contact/Contact.jsx
+++ vite-project/src/components/Contact/Contact.jsx
@@ -7,28 +7,33 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
+        <h2>Contact Me</h2>
         <p>Feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageURL("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageURL("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="https://www.linkedin.com/in/mehrnaz-mnh/?_l=en_US">
-            linkedin.com/mehrnaz-mnh
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageURL("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/mehrnaz-MNH">github.com/mehrnaz-MNH</a>
-        </li>
-      </ul>
+      <div>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img src={getImageURL("contact/emailIcon.png")} alt="Email icon" />
+            <a href="mailto:myemail@email.com">mnh.mehrnaz@gmail.com</a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageURL("contact/linkedinIcon.png")}
+              alt="LinkedIn icon"
+            />
+            <a href="https://www.linkedin.com/in/mehrnaz-mnh/?_l=en_US">
+              linkedin.com/mehrnaz-mnh
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageURL("contact/githubIcon.png")}
+              alt="Github icon"
+            />
+            <a href="https://github.com/mehrnaz-MNH">github.com/mehrnaz-MNH</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
